5ab4b60760960fc2b9ae1bb4aecfdcb97e72f2ff
TxHawks <tofu.hawks@gmail.com>
2017-03-29 11:32:41 +0300

    Fix build errors

--- config/webpack/prod.config.js
+++ config/webpack/prod.config.js
@@ -112,7 +112,6 @@ module.exports = function (env) {
       'jquery': "jQuery"
     }],
   })
-  const tsRuleindex = config.module.rules.findIndex(rule => rule.loader === 'awesome-typescript-loader')
-  config.module.rules[tsRuleindex].exclude = /\.spec\.ts$/;
+
   return config;
 };
